4c429aa69de84d74a4d8ffdd20adc4c6e83ba290
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2015-06-25 18:23:33 -0400

    BIG update :P

---
+++ domains/Python/Introduction/008-loops/solution.py
@@ -0,0 +1,4 @@
+n = int(raw_input())
+
+for i in range(n):
+	print pow(i,2)
---
+++ domains/Python/Introduction/009-whats-your-name/solution.py
@@ -0,0 +1,4 @@
+firstname = raw_input()
+lastname = raw_input()
+
+print "Hello %s %s! You just delved into python." % (firstname, lastname)
